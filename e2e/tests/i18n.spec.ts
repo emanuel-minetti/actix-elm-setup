08ebd10ec81ea1370229b97c469dc16b83b505d5
Emanuel Minetti <e.minetti@posteo.de>
2024-07-09 01:15:49 +0200

    Started e2e testing i18n.

--- e2e/tests/i18n.spec.ts
+++ e2e/tests/i18n.spec.ts
@@ -6,12 +6,23 @@ test.describe('language switcher', () => {
     });
 
     test('has at least german and english options', async ({page}) => {
-        const selectGermanEnglish = page.getByRole('combobox');
-        const options = await selectGermanEnglish.locator('option').all();
+        const headerElement = page.locator('header');
+        const langSwitcher = headerElement.getByRole('combobox');
+        const options = await langSwitcher.locator('option').all();
         const optionValues =
             await Promise.all( options.map(async option => await option.getAttribute('value')));
         expect(optionValues.length).toBeGreaterThanOrEqual(2);
         expect(optionValues).toContain('de');
         expect(optionValues).toContain('en');
-    })
+    });
+
+    test('works on switcher', async ({page}) => {
+        const headerElement = page.locator('header');
+        const langSwitcher = headerElement.getByRole('combobox');
+        await expect(langSwitcher).toHaveValue('en')
+        await expect(headerElement).toHaveText(/English/);
+        await langSwitcher.selectOption({value: 'de'});
+        await expect(langSwitcher).toHaveValue("de")
+        await expect(headerElement).toHaveText(/Deutsch/);
+    });
 });
