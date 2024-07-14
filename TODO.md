e770d18ba3e54961ba0075adcbc8c83c2ad30278
Emanuel Minetti <e.minetti@posteo.de>
2024-07-15 00:11:03 +0200

    Updated TODO.

--- TODO.md
+++ TODO.md
@@ -1,2 +1,9 @@
 ## Server
-- Add DB connection
+- Make login working.
+- Implement (basic) error handling.
+- Implement configuration.
+- Start using sessions.
+
+## Client
+- Adapt error handling to server errors
+- Use sessions.
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,14 +1,13 @@
 mod routes;
 
-use actix_web::{web, HttpServer};
 use actix_files::Files;
 use actix_web::web::Data;
-use deadpool_postgres::{Config as PoolConfig, ManagerConfig, RecyclingMethod, Runtime};
+use actix_web::{web, HttpServer};
 use deadpool_postgres::tokio_postgres::NoTls;
+use deadpool_postgres::{Config as PoolConfig, ManagerConfig, RecyclingMethod, Runtime};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-
     std::env::set_var("RUST_LOG", "debug");
     env_logger::init();
 
@@ -20,10 +19,12 @@ async fn main() -> std::io::Result<()> {
     db_conn_cfg.password = Some("aes".to_string());
     db_conn_cfg.connect_timeout = Some(std::time::Duration::from_secs(2));
     db_conn_cfg.manager = Some(ManagerConfig {
-        recycling_method: RecyclingMethod::Fast
+        recycling_method: RecyclingMethod::Fast,
     });
 
-    let db_pool = db_conn_cfg.create_pool(Some(Runtime::Tokio1), NoTls).unwrap();
+    let db_pool = db_conn_cfg
+        .create_pool(Some(Runtime::Tokio1), NoTls)
+        .unwrap();
 
     HttpServer::new(move || {
         actix_web::App::new()
@@ -35,13 +36,13 @@ async fn main() -> std::io::Result<()> {
                     .service(Files::new("/img", "../public/img"))
                     .service(Files::new("/lang", "../public/lang"))
                     .service(web::scope("/api")
-                         .route("/login", web::post().to(routes::login)))
+                        .route("/login", web::post().to(routes::login)))
                     .route("/favicon.ico", web::get().to(routes::return_favicon))
                     .route("/", web::get().to(routes::return_index))
-                    .route("/{route}", web::get().to(routes::return_index))
-            )})
-        .bind(("127.0.0.1", 8080))?
-        .run()
-        .await
+                    .route("/{route}", web::get().to(routes::return_index)),
+            )
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
-
