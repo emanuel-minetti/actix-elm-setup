28093fd482580ef75309ca6a0f6ae7f5bb4bd70c
Emanuel Minetti <e.minetti@posteo.de>
2024-07-25 23:58:08 +0200

    Repaired different responses for missing and empty fields to login request.

--- server/src/main.rs
+++ server/src/main.rs
@@ -7,7 +7,7 @@ use crate::configuration::get_configuration;
 use crate::validate_session::ValidateSession;
 use actix_files::Files;
 use actix_web::web::Data;
-use actix_web::{web, HttpServer};
+use actix_web::{web, HttpServer, error, HttpResponse};
 use sqlx::{Pool, Postgres};
 
 #[actix_web::main]
@@ -16,25 +16,37 @@ async fn main() -> std::io::Result<()> {
     env_logger::init();
 
     let configuration = get_configuration().expect("Couldn't read configuration file.");
-    let session_secret = bytes::Bytes::from(configuration.session_secret);
+
     let db_url = configuration.database.connection_string();
     let db_pool = Pool::<Postgres>::connect(db_url.as_str())
         .await
         .expect("Couldn't connect to database.");
 
     HttpServer::new(move || {
+        let session_secret = bytes::Bytes::from(configuration.session_secret);
+        let login_json_config = web::JsonConfig::default()
+            .limit(512)
+            .error_handler(|err, _| {
+                error::InternalError::from_response(err, HttpResponse::BadRequest().finish()).into()
+            });
+
         actix_web::App::new()
             .app_data(Data::new(db_pool.clone()))
-            .app_data(Data::new(session_secret.clone()))
             .service(
                 web::scope("")
                     .service(Files::new("/js", "../public/js"))
                     .service(Files::new("/css", "../public/css"))
                     .service(Files::new("/img", "../public/img"))
                     .service(Files::new("/lang", "../public/lang"))
-                    .route("/api/login", web::post().to(routes::login_handler))
+                    .service(
+                        web::scope("/api/login")
+                            .app_data(login_json_config)
+                            .app_data(Data::new(session_secret.clone()))
+                            .route("", web::post().to(routes::login_handler))
+                    )
                     .service(
                         web::scope("/api")
+                            .app_data(Data::new(session_secret.clone()))
                             .wrap(ValidateSession)
                             .route("/session", web::get().to(routes::session_handler)),
                     )
