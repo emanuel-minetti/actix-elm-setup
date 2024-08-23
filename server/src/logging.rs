c69e1f5cab989f25c228201ef6cb38f52c70afd0
Emanuel Minetti <e.minetti@posteo.de>
2024-08-24 01:31:28 +0200

    Still trying to delete old logs.

--- server/src/logging.rs
+++ server/src/logging.rs
@@ -1,8 +1,10 @@
 use crate::configuration::LogSettings;
+use chrono::Days;
 use log::{Level, LevelFilter, Metadata, Record, SetLoggerError};
 use sqlx::types::chrono::Utc;
-use std::fs::File;
+use std::fs::{read_dir, remove_file, File};
 use std::io::Write;
+use std::time::SystemTime;
 
 pub struct Logger {
     level: Level,
@@ -16,17 +18,11 @@ impl log::Log for Logger {
 
     fn log(&self, record: &Record) {
         if self.enabled(record.metadata()) {
-            let mut file = &mut self.file.try_clone().unwrap();
+            let file = &mut self.file.try_clone().unwrap();
             let now = Utc::now().format("%Y-%m-%d %H:%M:%S%.3f");
             println!("{} [{}]: {}", now, record.level(), record.args());
-            writeln!(
-                file,
-                "{} [{}]: {}",
-                now,
-                record.level(),
-                record.args()
-            )
-            .expect("Could not write to log file");
+            writeln!(file, "{} [{}]: {}", now, record.level(), record.args())
+                .expect("Could not write to log file");
             file.flush().unwrap();
         }
     }
@@ -36,15 +32,37 @@ impl log::Log for Logger {
 
 impl Logger {
     fn new(settings: LogSettings) -> Box<Self> {
-        let today = Utc::now().format("%Y-%m-%d");
-        let file_name = format!("log-{}.txt", today);
-        let file_path = settings.path_string + &*file_name;
-        let mut file = File::options()
+        let today = Utc::now();
+        let today_string = today.format("%Y-%m-%d");
+        let file_name = format!("log-{}.txt", today_string);
+        let file_path = settings.path_string.clone() + &*file_name;
+        let file = File::options()
             .append(true)
             .create(true)
             .open(file_path)
             .expect("Unable to open log file");
         // TODO delete outdated
+        // let filtered_log_files = read_dir(settings.path_string)
+        //     .unwrap()
+        //     .filter(move |entry| {
+        //         entry
+        //             .as_ref()
+        //             .unwrap()
+        //             .metadata()
+        //             .unwrap()
+        //             .created()
+        //             .unwrap()
+        //             .duration_since(SystemTime::UNIX_EPOCH)
+        //             .unwrap()
+        //             .as_secs()
+        //             <= today
+        //                 .checked_sub_days(Days::new(8))
+        //                 .unwrap()
+        //                 .timestamp()
+        //                 .try_into()
+        //                 .unwrap()
+        //     });
+        // filtered_log_files.for_each(move |file| { remove_file(file.unwrap().into()).expect("panic message");});
         Box::new(Logger {
             level: settings.max_level,
             file,
