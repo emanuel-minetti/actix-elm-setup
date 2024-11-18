a0a0d294089716723ef8acdbb5b13c2127bdda4a
Emanuel Minetti <e.minetti@posteo.de>
2024-11-18 21:59:37 +0100

    Refactored to use `Browser.application` aka SPA.

--- client-new/src/Main.elm
+++ client-new/src/Main.elm
@@ -1,24 +1,49 @@
 module Main exposing (main)
 
+import Array exposing (Array)
+import Browser exposing (Document)
 import Html exposing (..)
 import Html.Attributes exposing (..)
+import Url exposing (Url)
 
 
+view : Model -> Document Msg
 view _ =
-    div [ class "jumbotron" ]
-        [ h1 [] [ text "Welcome to Dunder Mifflin!" ]
-        , p []
-            [ text "Dunder Mifflin Inc. (stock symbol "
-            , strong [] [ text "DMI" ]
-            , text <|
-                """
-                ) is a micro-cap regional paper and office
-                supply distributor with an emphasis on servicing
-                small-business clients.
-                """
+    { title = "Actix Elm Setup"
+    , body =
+        [ div [ class "jumbotron" ]
+            [ h1 [] [ text "Welcome to Dunder Mifflin!" ]
+            , p []
+                [ text "Dunder Mifflin Inc. (stock symbol "
+                , strong [] [ text "DMI" ]
+                , text <|
+                    """
+                                 ) is a micro-cap regional paper and office
+                                 supply distributor with an emphasis on servicing
+                                 small-business clients.
+                                 """
+                ]
             ]
         ]
+    }
 
 
+type alias Model =
+    {}
+
+
+type Msg
+    = ChangedUrl Url
+    | ClickedLink Browser.UrlRequest
+
+
+main : Program (Array String) Model Msg
 main =
-    view ""
+    Browser.application
+        { init = \_ _ _ -> ( Model, Cmd.none )
+        , update = \_ _ -> ( Model, Cmd.none )
+        , subscriptions = \_ -> Sub.none
+        , view = view
+        , onUrlRequest = ClickedLink
+        , onUrlChange = ChangedUrl
+        }
