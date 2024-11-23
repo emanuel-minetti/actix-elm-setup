9f7638750d749e17e5820c1970b1e0f50327bae2
Emanuel Minetti <e.minetti@posteo.de>
2024-11-24 00:33:09 +0100

    I18n is basically working.

--- client-new/src/Main.elm
+++ client-new/src/Main.elm
@@ -5,7 +5,87 @@ import Browser exposing (Document)
 import Browser.Navigation as Nav
 import Html exposing (..)
 import Html.Attributes exposing (..)
+import Http
+import I18Next exposing (Translations, t, translationsDecoder)
 import Url exposing (Url)
+
+
+type alias Model =
+    { lang : String
+    , t : Maybe Translations
+    }
+
+
+type Msg
+    = ChangedUrl Url
+    | ClickedLink Browser.UrlRequest
+    | GotTranslation (Result Http.Error Translations)
+
+
+main : Program (Array String) Model Msg
+main =
+    Browser.application
+        { init = init
+        , update = update
+        , subscriptions = \_ -> Sub.none
+        , view = view
+        , onUrlRequest = ClickedLink
+        , onUrlChange = ChangedUrl
+        }
+
+
+init : Array String -> Url -> Nav.Key -> ( Model, Cmd Msg )
+init flags _ _ =
+    let
+        defaultLang =
+            "en"
+
+        langFromBrowser =
+            Array.get 0 flags
+                |> Maybe.withDefault defaultLang
+                |> String.slice 0 2
+
+        lang =
+            if langFromBrowser == "en" || langFromBrowser == "de" then
+                langFromBrowser
+
+            else
+                defaultLang
+
+        initialModel =
+            { lang = lang
+            , t = Nothing
+            }
+    in
+    ( initialModel, loadTranslation lang )
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        GotTranslation result ->
+            case result of
+                Err _ ->
+                    ( model, Cmd.none )
+
+                Ok translation ->
+                    ( { model | t = Just translation }, Cmd.none )
+
+        _ ->
+            ( model, Cmd.none )
+
+
+loadTranslation : String -> Cmd Msg
+loadTranslation lang =
+    Http.request
+        { method = "GET"
+        , url = "http://127.0.0.1:8080/lang/main." ++ lang ++ ".json"
+        , headers = [ Http.header "Accept" "application/json" ]
+        , body = Http.emptyBody
+        , expect = Http.expectJson GotTranslation translationsDecoder
+        , timeout = Nothing
+        , tracker = Nothing
+        }
 
 
 view : Model -> Document Msg
@@ -28,51 +108,13 @@ view model =
                 ]
             ]
         , div [ class "container" ]
-            [ text ("Your preferred Language is: " ++ model.lang) ]
+            [ text <|
+                case model.t of
+                    Nothing ->
+                        "" ++ model.lang
+
+                    Just res ->
+                        t res "yourPreferredLang" ++ model.lang
+            ]
         ]
     }
-
-
-type alias Model =
-    { lang : String
-    }
-
-
-type Msg
-    = ChangedUrl Url
-    | ClickedLink Browser.UrlRequest
-
-
-main : Program (Array String) Model Msg
-main =
-    Browser.application
-        { init = init
-        , update = \_ model -> ( model, Cmd.none )
-        , subscriptions = \_ -> Sub.none
-        , view = view
-        , onUrlRequest = ClickedLink
-        , onUrlChange = ChangedUrl
-        }
-
-
-init : Array String -> Url -> Nav.Key -> ( Model, Cmd Msg )
-init flags _ _ =
-    let
-        defaultLang =
-            "en"
-
-        langFromBrowser =
-            String.slice 0 2 <| Maybe.withDefault defaultLang <| Array.get 0 flags
-
-        lang =
-            if langFromBrowser == "en" || langFromBrowser == "de" then
-                langFromBrowser
-
-            else
-                defaultLang
-
-        initialModel =
-            { lang = lang
-            }
-    in
-    ( initialModel, Cmd.none )
